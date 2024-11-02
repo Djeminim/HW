3f17f078414cbbec8846fa0a4c0bfa09455bbe1b
Artem Zuev <115079882+Djeminim@users.noreply.github.com>
2024-11-02 15:45:51 +0300

    Add files via upload

---
+++ HW3/task3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float a,b,c;
+    float x;
+    scanf("%f %f %f" , &a,&b,&c); 
+    x = (a+b+c)/3;
+    printf("%.2f \n", x );
+
+
+}
+
+
+
+
---
+++ HW3/task5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+	int a,b,c,value;
+    int x;
+    scanf("%i", &value); 
+    a = value/100;
+    b = (value/10)-10;
+    c = a-(x*10);
+    x = a*b*c;
+      
+    printf("%i \n", x );
+}
+
+
+  /*  int a,b,c;
+    int x;
+    scanf("%i", &a); 
+    a = a/10;
+    b = a/100;
+    c = a-(x*10);
+    x = a*b*c;
+      
+    printf("%i \n", x );
+    */
+    
