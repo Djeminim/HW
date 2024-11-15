5dc9c9b8ef6ac9ce356712cb62a1b86a521b0f0f
Artem Zuev <115079882+Djeminim@users.noreply.github.com>
2024-11-15 18:01:40 +0300

    1
    1

--- HW6/task_10.c
+++ HW6/task_10.c
@@ -1,23 +1,33 @@
-
 #include <stdio.h>
 
 void print_simple(int n)
 {
-	
-	for (int i=1;i<=n;i++)
-	{
-		if ((n%i)==0)
-		printf("%i ", i);
+	int f=2;
 
+	while (n/f>=1 )
+	{
+		if (n%f==0)
+		{
+			while (n%f==0)
+			{
+				printf ("%i ", f);
+				n=n/f;			
+			}		
+		}
+		
+		else if (n%f!=0)
+		f=f+1;
+		
 	}
 
 }
 
-int main(int argc, char **argv)
+int main()
 {
 	int n;
 	scanf("%i", &n);
 	print_simple(n);
 	return 0;
+	
 }
 
--- HW6/task_11.c
+++ HW6/task_11.c
@@ -1,28 +1,59 @@
 
 #include <stdio.h>
 
-void print_simple(int n)
-{
-	int f;
+ void nod(int a, int b)
+{	
+	int maxA,maxB;
+	int f,c=2;
+	
+	if (a>b)f=a;
+	else f=b;
 
-	for (int i=2;i<=f;i++)
+	for(int i=0;i<=f;i++)
 	{
-		
-		if ((f%i)==0)
-		{	
-			f=f/i;
-			printf("%i ", i);
-			--i;
+		if(a%b==0)
+		{
+			maxA=a/b;
+		}
+		else if (b%a==0)
+		{
+			maxB=b/a;
+		}
+		else
+		{
+			if (a%b!=0 && b%a!=0)
+			{
+				while (a%b!=0 || b%a!=0)
+				{
+					a=a/c;
+					b=b/c;
+					if (a%b==0)
+					{
+						
+					}
+				}
+			}
 		}
 	}
 
+	while (a/f>=1 && b/f>=1)
+	{
+		if (a%f==0 && b%f==0)
+		{
+			printf ("%i ", f);
+			maxA=a/f;
+			maxB=b/f;					
+		}		
+		f=f+1;	
+	}
 }
 
-int main(int argc, char **argv)
+int main()
 {
-	int n;
-	scanf("%i", &n);
-	print_simple(n);
+	int a,b;
+	scanf("%i %i", &a,&b);
+	nod(a,b);
 	return 0;
+	
 }
 
