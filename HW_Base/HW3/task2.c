2c84574d6dc760384af4cec4a709c74683410b5d
Artem <artemo1010@mail.ru>
2025-02-22 14:37:51 +0300

    Sort

---
+++ HW_Base/HW3/task2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x,a,b,c,x2;
+    scanf("%d %d %d" , &a,&b,&c); 
+    x = a+b+c;
+    x2 = a*b*c;
+    printf("%d+%d+%d=%d \n", a,b,c,x );
+    printf("%d*%d*%d=%d \n", a,b,c,x2 );
+
+}
+
+
---
+++ HW_Base/HW3/task4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a,b;
+    int x;
+    scanf("%i %i" , &a,&b); 
+    x = (a-b);
+    printf("%i \n", x );
+}
